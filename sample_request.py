cd4f34e25d4df0fd8ba69718f80cf90648162e91
datacamp489 <98825134+datacamp489@users.noreply.github.com>
2022-04-28 16:55:24 +0200

    url added

--- sample_request.py
+++ sample_request.py
@@ -2,7 +2,7 @@
 
 import requests
 
-url = "https://heroku.."
+url = "https://census-ml.herokuapp.com/predict"
 data = {'age': 37, 'workclass': 'Private', 'fnlgt': 321943, 'education': 'Prof-school', 'education_num': 15, 'marital_status': 'Married-civ-spouse', 'occupation': 'Prof-specialty',
         'relationship': 'Husband', 'race': 'White', 'sex': 'Male', 'capital_gain': 0, 'capital_loss': 0, 'hours_per_week': 40, 'native_country': 'United-States'}
 response = requests.post(url, json=data)
